3cee577cb5cacbfd1f458835df9050e404cb28c1
rorozoamuhammad <ilhammoch2006@gmail.com>
2022-06-11 12:33:50 +0700

    mengubah javascript

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@ const desktop = document.querySelector('.desktop');
 const mobile = document.querySelector('.mobile');
 const form = document.querySelector('form');
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (e) => {
     if(txt.value === '') {
         txt.dataset.error = 'error';
         desktop.dataset.error = 'error';
         mobile.dataset.error = 'error';
     } else {
-        txt.database.error = 'ok';
+        txt.dataset.error = 'ok';
         desktop.dataset.error = 'ok';
         mobile.dataset.error = 'ok';
+        e.preventDefault();
     }
 });
